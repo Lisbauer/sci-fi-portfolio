9285171194c72b535a891776836fc6b51ce101b7
Lisbauer <lisa.bauer@hotmail.com>
2024-06-22 01:10:52 -0300

    bgimages solving problem

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  }
+});
