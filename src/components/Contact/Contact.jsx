4d8065524763481f709952506549c30d2c4f8da0
Lisbauer <lisa.bauer@hotmail.com>
2024-06-26 04:44:06 -0300

    Inputs validator

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     email: "",
     message: "",
   });
-  const [statusMessage, setStatusMessage] = useState("");
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -22,20 +22,39 @@ const Contact = () => {
     }));
   };
 
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.subject) newErrors.subject = "Subject is missing";
+    if (!formData.name) newErrors.name = "Name is missing";
+    if (!formData.email) {
+      newErrors.email = "Please enter a valid Email";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Please enter a valid Email";
+    }
+    if (!formData.message) newErrors.message = "Message is missing";
+    return newErrors;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+    setErrors({});
     try {
       const response = await axios.post('http://localhost:3001/send', formData);
 
       if (response.status === 200) {
         setTimeout(() => {
           navigate('/messagesent');
-        }, 500); 
+        }, 50); 
       } else {
-        setStatusMessage("Error al enviar el email");
+        setErrors({ submit: "Error al enviar el email" });
       }
     } catch (error) {
-      setStatusMessage("Ocurrió un error al enviar el email: " + error.message);
+      setErrors({ submit: "Ocurrió un error al enviar el email: " + error.message });
     }
   };
 
@@ -44,17 +63,67 @@ const Contact = () => {
       <h1 className="font-audiowide text-[#fffc00] text-[2em] md:text-[2em] lg:text-[3em] w-[300px] lg:w-[400px] uppercase mb-7 text-center">
         Contact me
       </h1>
-      <form className="flex flex-col items-center justify-center gap-6" onSubmit={handleSubmit}>
-        <Input label="Asunto" name="subject" color="white" className="h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px]" value={formData.subject} onChange={handleChange} />
-        <Input label="Nombre Completo" name="name" color="white" className="h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px]" value={formData.name} onChange={handleChange} />
-        <Input label="Correo Electrónico" name="email" color="white" className="h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px]" value={formData.email} onChange={handleChange} />
-        <Input label="Mensaje" name="message" color="white" className="h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px]" value={formData.message} onChange={handleChange} />
-        <Button type="submit" className="w-64 font-audiowide text-[#fffc00] text-[0.9em] sm:text-[1.2em] relative overflow-hidden bg-gradient-to-l from-purple-600 to-pink-600">
+      <form className="flex flex-col items-center justify-center gap-6 font-kanit" onSubmit={handleSubmit}>
+        <Input 
+          label="Asunto" 
+          name="subject" 
+          color="white" 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit bg-black-60 focus:bg-black-60  ${errors.subject ? 'border-2 border-solid border-violet-500' : ''}`} 
+          value={formData.subject} 
+          onChange={handleChange} 
+          
+        />
+        
+        <Input 
+          label="Nombre Completo" 
+          name="name" 
+          color="white" 
+          className={`font-kanit h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.name ? 'border-2 border-solid border-violet-500' : ''}`} 
+          value={formData.name} 
+          onChange={handleChange} 
+        />
+        
+        <Input 
+          label="Correo Electrónico" 
+          name="email" 
+          color="white" 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.email ? 'border-2 border-solid border-violet-500' : ''}`} 
+          value={formData.email} 
+          onChange={handleChange} 
+          
+        />
+        
+        <Input 
+          label="Mensaje" 
+          name="message" 
+          color="white" 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.message ? 'border-2 border-solid border-violet-500' : ''}`} 
+          value={formData.message} 
+          onChange={handleChange} 
+        />
+        
+        <Button 
+          type="submit" 
+          className="w-64 font-audiowide text-[#fffc00] text-[0.9em] sm:text-[1.2em] relative overflow-hidden bg-gradient-to-l from-purple-600 to-pink-600"
+        >
           <span className="relative z-10">Enviar</span>
           <span className="absolute inset-0 bg-gradient-to-l from-pink-600 to-purple-600 opacity-0 hover:opacity-100 transition-opacity duration-1000"></span>
         </Button>
+        
+        {Object.keys(errors).length > 0 && (
+          <div className="mt-2 text-sm text-white font-kanit">
+            Please fix the following errors before submitting:
+            <ul className="list-disc list-inside">
+              {errors.subject && <li className="text-white">{errors.subject}</li>}
+              {errors.name && <li className="text-white">{errors.name}</li>}
+              {errors.email && <li className="text-white">{errors.email}</li>}
+              {errors.message && <li className="text-white">{errors.message}</li>}
+            </ul>
+          </div>
+        )}
+
+        {errors.submit && <p className="text-violet-500 text-sm mt-2">{errors.submit}</p>}
       </form>
-      {statusMessage && <p className="text-white mt-4">{statusMessage}</p>}
     </div>
   );
 };
