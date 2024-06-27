43b12155fa72b2faecb46c59606faaae6bd57725
Lisbauer <lisa.bauer@hotmail.com>
2024-06-27 02:46:28 -0300

    Inputs into english

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -59,45 +59,45 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact" className="flex flex-col items-center justify-center h-[auto] mt-44 md:mt-48 xl:mt-24 2xl:mt-32  md border-2 border-solid border-yellow-400 rounded-lg px-[4em] sm:px-[13em] py-[5em] lg:p-[10em] bg-black-60  desktop:mt-36 lg:px-10 xl:px-20 sm:w-[40%] md:w-[50%] w-[70%]" data-aos="fade-down">
+    <div id="contact" className="flex flex-col items-center justify-center h-[auto] mt-44 md:mt-48 xl:mt-24 2xl:mt-12     border-2 border-solid border-yellow-400 rounded-lg px-[4em] sm:px-[13em] py-[5em] lg:px-[18em] lg:p-[10em] bg-black-60 desktop:mt-48 xl:px-20 sm:w-[40%] md:w-[50%] w-[70%]" >
       <h1 className="font-audiowide text-[#fffc00] text-[2em] md:text-[2em] lg:text-[3em] w-[300px] lg:w-[400px] uppercase mb-7 text-center">
         Contact me
       </h1>
       <form className="flex flex-col items-center justify-center gap-6 font-kanit" onSubmit={handleSubmit}>
         <Input 
-          label="Asunto" 
+          label="Subject" 
           name="subject" 
           color="white" 
-          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit bg-black-60 focus:bg-black-60  ${errors.subject ? 'border-2 border-solid border-violet-500' : ''}`} 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[550px] input-font-kanit bg-black-60 focus:bg-black-60  ${errors.subject ? 'border-2 border-solid border-violet-500' : ''}`} 
           value={formData.subject} 
           onChange={handleChange} 
           
         />
         
         <Input 
-          label="Nombre Completo" 
+          label="Full Name" 
           name="name" 
           color="white" 
-          className={`font-kanit h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.name ? 'border-2 border-solid border-violet-500' : ''}`} 
+          className={`font-kanit h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[550px] input-font-kanit ${errors.name ? 'border-2 border-solid border-violet-500' : ''}`} 
           value={formData.name} 
           onChange={handleChange} 
         />
         
         <Input 
-          label="Correo Electrónico" 
+          label="Email" 
           name="email" 
           color="white" 
-          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.email ? 'border-2 border-solid border-violet-500' : ''}`} 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[550px] input-font-kanit ${errors.email ? 'border-2 border-solid border-violet-500' : ''}`} 
           value={formData.email} 
           onChange={handleChange} 
           
         />
         
         <Input 
-          label="Mensaje" 
+          label="Message" 
           name="message" 
           color="white" 
-          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[450px] input-font-kanit ${errors.message ? 'border-2 border-solid border-violet-500' : ''}`} 
+          className={`h-[3em] text-[1.1em] w-[300px] md:w-[400px] lg:w-[550px] input-font-kanit ${errors.message ? 'border-2 border-solid border-violet-500' : ''}`} 
           value={formData.message} 
           onChange={handleChange} 
         />
@@ -106,7 +106,7 @@ const Contact = () => {
           type="submit" 
           className="w-64 font-audiowide text-[#fffc00] text-[0.9em] sm:text-[1.2em] relative overflow-hidden bg-gradient-to-l from-purple-600 to-pink-600"
         >
-          <span className="relative z-10">Enviar</span>
+          <span className="relative z-10">Send</span>
           <span className="absolute inset-0 bg-gradient-to-l from-pink-600 to-purple-600 opacity-0 hover:opacity-100 transition-opacity duration-1000"></span>
         </Button>
         
