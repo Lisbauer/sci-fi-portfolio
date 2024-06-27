67dd8e034a2c26e2e1a0bd465943e430224f3de0
Lisbauer <lisa.bauer@hotmail.com>
2024-06-27 17:53:30 -0300

    disabled translator

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import MessageReceived from "./components/MessageReceived/MessageReceived.jsx";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App" translate="no">
         <div id="message">
           <Routes>
             <Route path="/messagesent" element={<MessageReceived />} />
